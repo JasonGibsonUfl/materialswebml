569f7da75b6fa782bcce876f95d7f44e22fe8a53
Jason Gibson <jasongibson@ufl.edu>
2021-03-11 19:09:50 -0500

    add docstrings

--- mlmodel/urls.py
+++ mlmodel/urls.py
@@ -8,11 +8,6 @@ router.register(r"MLModel", views.MLModelViewSet)
 router.register(r"MaterialSystem", views.MaterialsSystemViewSet)
 router.register(r"Element", views.ElementViewSet)
 router.register(r'TrainingData', views.TrainingDataViewSet)
-# router.register(r"dual_coef_test", views.dual_coef_testViewSet)
-# router.register(r"dual_coef_row", views.dual_coef_rowViewSet)
-# router.register(r"dual_coef_weight", views.dual_coef_weightViewSet)
-# router.register(r'Descriptors', views.DescriptorsViewSet)
-
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
