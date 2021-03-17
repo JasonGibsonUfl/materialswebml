7bc3483e906edcdcfee4ddb8080c798ebd8bd8c0
JasonGibsonUfl <jasongibson@ufl.edu>
2021-03-17 12:52:08 -0400

    move custom fields to custom.py

---
+++ mlmodel/custom.py
@@ -0,0 +1,82 @@
+"""
+contians custom fields for models
+"""
+from django.db import models
+import pickle
+import numpy as np
+import ast
+from collections import defaultdict
+
+class NumpyArrayField(models.TextField):
+    description = "Stores a Numpy ndarray."
+    #__metaclass__ = models.SubfieldBase
+
+    def __init__(self, *args, **kwargs):
+        super(NumpyArrayField, self).__init__(*args, **kwargs)
+
+    def to_python(self, value):
+        if isinstance(value, list):
+            value = np.array(list)
+        if isinstance(value, np.ndarray):
+            return value
+
+        if not value:
+            return np.array([])
+
+        return np.array(pickle.loads(str(value)))
+
+    def get_prep_value(self, value):
+        if isinstance(value, list):
+            return str(value)
+        elif isinstance(value, np.ndarray):
+            return str(value.tolist())
+            #return pickle.dumps(value.tolist())
+        else:
+            raise TypeError('%s is not a list or numpy array' % value)
+
+class DictField(models.TextField):
+    #__metaclass__ = models.SubfieldBase
+    description = "Stores a python dictionary"
+
+    def __init__(self, *args, **kwargs):
+        super(DictField, self).__init__(*args, **kwargs)
+
+    def to_python(self, value):
+        if not value:
+            value = {}
+
+        if isinstance(value, dict):
+            return value
+
+        return ast.literal_eval(value)
+
+    def get_prep_value(self, value):
+        if value is None:
+            return value
+
+        if isinstance(value, defaultdict):
+            value = dict(value)
+
+        return str(value)
+
+    def value_to_string(self, obj):
+        value = self._get_val_from_obj(obj)
+        return self.get_db_prep_value(value)
+
+class JSONField(models.TextField):
+    #__metaclass__ = models.SubfieldBase
+    description = "Stores a python dictionary"
+
+    def __init__(self, *args, **kwargs):
+        super(JSONField, self).__init__(*args, **kwargs)
+
+    def to_python(self, value):
+        if not value:
+            return value
+        if not isinstance(value, str):
+            return value
+        return json.loads(value)
+
+    def get_prep_value(self, value):
+        print('get prep value', value)
+        return json.dumps(value)
--- mlmodel/models.py
+++ mlmodel/models.py
@@ -3,83 +3,9 @@ import pickle
 import base64
 import codecs
 import numpy as np
-from collections import defaultdict
 import ast
 import json
-class NumpyArrayField(models.TextField):
-    description = "Stores a Numpy ndarray."
-    #__metaclass__ = models.SubfieldBase
-
-    def __init__(self, *args, **kwargs):
-        super(NumpyArrayField, self).__init__(*args, **kwargs)
-
-    def to_python(self, value):
-        if isinstance(value, list):
-            value = np.array(list)
-        if isinstance(value, np.ndarray):
-            return value
-
-        if not value:
-            return np.array([])
-
-        return np.array(pickle.loads(str(value)))
-
-    def get_prep_value(self, value):
-        if isinstance(value, list):
-            return str(value)
-        elif isinstance(value, np.ndarray):
-            return str(value.tolist())
-            #return pickle.dumps(value.tolist())
-        else:
-            raise TypeError('%s is not a list or numpy array' % value)
-
-class DictField(models.TextField):
-    #__metaclass__ = models.SubfieldBase
-    description = "Stores a python dictionary"
-
-    def __init__(self, *args, **kwargs):
-        super(DictField, self).__init__(*args, **kwargs)
-
-    def to_python(self, value):
-        if not value:
-            value = {}
-
-        if isinstance(value, dict):
-            return value
-
-        return ast.literal_eval(value)
-
-    def get_prep_value(self, value):
-        if value is None:
-            return value
-
-        if isinstance(value, defaultdict):
-            value = dict(value)
-
-        return str(value)
-
-    def value_to_string(self, obj):
-        value = self._get_val_from_obj(obj)
-        return self.get_db_prep_value(value)
-
-class JSONField(models.TextField):
-    #__metaclass__ = models.SubfieldBase
-    description = "Stores a python dictionary"
-
-    def __init__(self, *args, **kwargs):
-        super(JSONField, self).__init__(*args, **kwargs)
-
-    def to_python(self, value):
-        if not value:
-            return value
-        if not isinstance(value, str):
-            return value
-        return json.loads(value)
-
-    def get_prep_value(self, value):
-        print('get prep value', value)
-        return json.dumps(value)
-
+from mlmodel.custom import NumpyArrayField, DictField
 
 class Element(models.Model):
     """
@@ -148,11 +74,10 @@ class MLModel(models.Model):
     target_property = models.CharField(max_length=25,choices=TARGET_PROBERTY_CHOICES)
     material_system = models.ForeignKey(MaterialSystem, related_name='mlmodel', on_delete=models.CASCADE)
     name = models.TextField()
+    DOI = models.TextField()
     def __str__(self):
         string = ''
         return '-'.join([str(element) for element in self.material_system.elements.all()]) + '\t' + str(self.target_property)
-
-
 
 class SVRModel(models.Model):
     """
